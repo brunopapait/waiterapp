f9faa39e00019712d3ec0f9abe2d1da1f90faf9e
brunopapait <brunohpapait@gmail.com>
2022-11-16 23:19:53 -0300

    feat: finish useCase category

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -4,6 +4,10 @@ import { Category } from "../../models/Category";
 export async function createCategories(req: Request, res: Response) {
   const { icon, name } = req.body;
 
-  const category = await Category.create({ icon, name });
-  return res.json(category);
+  try {
+    const category = await Category.create({ icon, name });
+    return res.status(201).json(category);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 }
--- api/src/app/useCases/categories/listCategories.ts
+++ api/src/app/useCases/categories/listCategories.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import { Category } from "../../models/Category";
 
 export async function listCategories(req: Request, res: Response) {
-  const categories = await Category.find();
-
-  return res.status(200).json(categories);
+  try {
+    const categories = await Category.find();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 }
