d22acc6698fc20bbe77adad775c54812d49d06da
brunopapait <brunohpapait@gmail.com>
2023-01-17 22:39:39 -0300

    feat: up

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,16 +3,17 @@ import mongoose from 'mongoose';
 import path from 'node:path';
 import { router } from './router';
 
+const PORT = 3001;
+
 mongoose.connect('mongodb://localhost:27017')
-.then(() => {
-  const app = express();
+  .then(() => {
+    const app = express();
 
-  app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-  app.use(express.json());
-  app.use(router);
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
 
-  const port = 3001;
-  app.listen(3001, () => console.log(`Server is running on http://localhost:${port}`));
-})
-.catch(err => console.error('Could not connect to MongoDB...', err));
+    app.listen(3001, () => console.log(`Server is running on http://localhost:${PORT}`));
+  })
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
