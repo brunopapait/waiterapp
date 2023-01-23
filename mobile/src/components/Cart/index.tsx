6ad90aec1d32d46160f0c01aea4c72eedb15910f
brunopapait <brunohpapait@gmail.com>
2023-01-22 23:37:54 -0300

    feat: progressing in cart component adding new features

--- mobile/src/components/Cart/index.tsx
+++ mobile/src/components/Cart/index.tsx
@@ -1,54 +1,104 @@
+import { useState } from 'react';
 import { FlatList, TouchableOpacity } from 'react-native';
 import { CartItem } from '../../types/CartItem';
+import { Product } from '../../types/Product';
 import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Button';
 import { MinusCircle } from '../Icons/MinusCircle';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { OrderConfirmedModal } from '../OrderConfirmedModal';
 import { Text } from '../Text';
-import { Actions, Image, Item, ProductContainer, QuantityContainer, ProductDetails } from './styles';
+import { Actions, Image, Item, ProductContainer, QuantityContainer, ProductDetails, Summary, TotalContainer } from './styles';
 
 interface CartProps {
   cartItems: CartItem[];
+  onAdd: (product: Product) => void;
+  onDecrement: (product: Product) => void;
+  onConfirmOrder: () => void;
 }
 
-export default function Cart({ cartItems }: CartProps) {
+export default function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder }: CartProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isLoading] = useState(false);
+
+  const isCartIsEmpty = cartItems.length === 0;
+
+  const total = cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.product.price * cartItem.quantity;
+  }, 0);
+
+  function handleConfirmOrder() {
+    setIsModalVisible(true);
+  }
+
+  function handleOk() {
+    setIsModalVisible(false);
+    onConfirmOrder();
+  }
+
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={item => item.product._id}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item: cartItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image
-              source={{ uri: `http://192.168.0.102:3001/uploads/${cartItem.product.imagePath}` }}
-            />
+    <>
+      {!isCartIsEmpty && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={item => item.product._id}
+          showsVerticalScrollIndicator={false}
+          style={{ marginBottom: 20, maxHeight: 150 }}
+          renderItem={({ item: cartItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={{ uri: `http://192.168.0.102:3001/uploads/${cartItem.product.imagePath}` }}
+                />
 
-            <QuantityContainer>
-              <Text color='#666' size={14}>{cartItem.quantity}x</Text>
-            </QuantityContainer>
+                <QuantityContainer>
+                  <Text color='#666' size={14}>{cartItem.quantity}x</Text>
+                </QuantityContainer>
 
-            <ProductDetails>
-              <Text weight='600' size={14}>{cartItem.product.name}</Text>
-              <Text
-                style={{ marginTop: 4 }}
-                color='#666'
-                size={14}
-              >
-                {formatCurrency(cartItem.product.price)}
-              </Text>
-            </ProductDetails>
-          </ProductContainer>
-          <Actions>
-            <TouchableOpacity style={{ marginRight: 24 }}>
-              <PlusCircle />
-            </TouchableOpacity>
+                <ProductDetails>
+                  <Text weight='600' size={14}>{cartItem.product.name}</Text>
+                  <Text
+                    style={{ marginTop: 4 }}
+                    color='#666'
+                    size={14}
+                  >
+                    {formatCurrency(cartItem.product.price * cartItem.quantity)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
+              <Actions>
+                <TouchableOpacity style={{ marginRight: 24 }} onPress={() => onAdd(cartItem.product)}>
+                  <PlusCircle />
+                </TouchableOpacity>
 
-            <TouchableOpacity>
-              <MinusCircle />
-            </TouchableOpacity>
-          </Actions>
-        </Item>
+                <TouchableOpacity onPress={() => onDecrement(cartItem.product)}>
+                  <MinusCircle />
+                </TouchableOpacity>
+              </Actions>
+            </Item>
+          )}
+        />
       )}
-    />
+
+      <Summary>
+        <TotalContainer>
+          <Text color={!isCartIsEmpty ? '#666' : '#999'}>{!isCartIsEmpty ? 'Total' : 'Seu carrinho está vazio'}</Text>
+          {!isCartIsEmpty && <Text weight='600' size={18}>{formatCurrency(total)}</Text>}
+        </TotalContainer>
+
+        <Button
+          disabled={isCartIsEmpty}
+          onPress={handleConfirmOrder}
+          loading={isLoading}
+        >
+          Confirmar Pedido
+        </Button>
+      </Summary>
+
+      <OrderConfirmedModal
+        visible={isModalVisible}
+        onOk={handleOk}
+      />
+    </>
   );
 }
